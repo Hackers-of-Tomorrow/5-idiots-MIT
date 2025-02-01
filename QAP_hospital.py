8741447b9380907e73a98abf5132f00fc3065a1e
mero <567mero345@gmail.com>
2025-02-01 15:59:31 -0500

    Added penalties for distance traveled

--- QAP_hospital.py
+++ QAP_hospital.py
@@ -21,39 +21,47 @@ N_rooms = 3
 N_supply = 2
 max_flow = 100
 max_distance = 1
-
+time_steps = 10
 
 facilities = range(N_rooms + N_supply)
 
 # Generate the non linear quantum model
 model = Model()
 
-flow = helper.random_matrix(N_supply, N_rooms, max_flow)
-distance = helper.random_matrix(N_supply, N_rooms, max_distance)
+flow = helper.random_time_matrix(time_steps, N_supply, N_rooms, max_flow)
+room_supply_distance = helper.random_matrix(N_supply, N_rooms, max_distance)
+room_room_distance = helper.random_symmetric_matrix(N_rooms, 0, max_distance)
+supply_supply_distance = helper.random_symmetric_matrix(N_supply, 0, max_distance)
 
 model_flow = model.constant(flow)
-model_distance = model.constant(distance)
+model_rs_distance = model.constant(room_supply_distance)
+model_rr_distance = model.constant(room_room_distance)
+model_ss_distance = model.constant(supply_supply_distance)
 
 permutation_rooms = model.list(N_rooms)
 permutation_supply = model.list(N_supply)
 
-print(flow)
-print(distance)
-
 # Create the objective function
-
-objective_function = (model_flow[permutation_supply][:, permutation_rooms] * model_distance).sum()
-
-# Minimize the model using the objective function
-
-model.minimize(objective_function)
-
-# Sample the model to see the output
+old_room_permutation = model.list(N_rooms)
+old_supply_permutation = model.list(N_supply)
 
 sampler = LeapHybridNLSampler()
-sampler.sample(model)
 
 # Output the results
-with model.lock():
-    print(list(sym.state(0) for sym in model.iter_decisions()))
-    print(model.objective.state(0))
+
+for i in range(time_steps):
+
+    cost = (model_flow[i][permutation_supply][:, permutation_rooms] * model_rs_distance).sum()
+    room_penalty = (model_rr_distance[permutation_rooms][old_room_permutation]).sum()
+    supply_penalty = (model_ss_distance[permutation_supply][old_supply_permutation]).sum()
+    model.minimize(cost + room_penalty + supply_penalty)
+    sampler.sample(model)
+    with model.lock():
+        print(list(sym.state(0) for sym in model.iter_decisions()))
+        states = list(sym for sym in model.iter_decisions())
+        old_supply_permutation = states[3]
+        old_room_permutation = states[2]
+        print(model.objective.state(0))
+
+
+# Sample the model to see the output
