81dfeb9879bbbef009e299c6a5a14ab4f59bf186
mero <567mero345@gmail.com>
2025-02-01 16:24:02 -0500

    Added quantum function

--- QAP_hospital.py
+++ QAP_hospital.py
@@ -23,50 +23,58 @@ max_flow = 100 #maximum value of flow from a given supply closet to each room
 max_distance = 1 #maximum distance between any two points in the graph
 time_steps = 10 #number of times the flow matrix changes
 
-# Generate the non linear quantum model
-model = Model()
 
-#define flow matrix and distance matrices
-#we use three small distance matrices instead of one large one since we do not need all the distances
-flow = helper.random_time_matrix(time_steps, N_supply, N_rooms, max_flow)
-room_supply_distance = helper.random_matrix(N_supply, N_rooms, max_distance)
-room_room_distance = helper.random_symmetric_matrix(N_rooms, 0, max_distance)
-supply_supply_distance = helper.random_symmetric_matrix(N_supply, 0, max_distance)
 
-model_flow = model.constant(flow)
-model_rs_distance = model.constant(room_supply_distance)
-model_rr_distance = model.constant(room_room_distance)
-model_ss_distance = model.constant(supply_supply_distance)
+def quantum_solution(N_rooms, N_supply, max_flow, max_distance, time_steps):
 
-#create two lists for permutations of rooms and supply closets
-permutation_rooms = model.list(N_rooms)
-permutation_supply = model.list(N_supply)
+    costs = 0
+    # Generate the non linear quantum model
+    model = Model()
 
-#store the previous permutation
-old_room_permutation = model.list(N_rooms)
-old_supply_permutation = model.list(N_supply)
+    #define flow matrix and distance matrices
+    #we use three small distance matrices instead of one large one since we do not need all the distances
+    flow = helper.random_time_matrix(time_steps, N_supply, N_rooms, max_flow)
+    room_supply_distance = helper.random_matrix(N_supply, N_rooms, max_distance)
+    room_room_distance = helper.random_symmetric_matrix(N_rooms, 0, max_distance)
+    supply_supply_distance = helper.random_symmetric_matrix(N_supply, 0, max_distance)
 
-sampler = LeapHybridNLSampler()
+    model_flow = model.constant(flow)
+    model_rs_distance = model.constant(room_supply_distance)
+    model_rr_distance = model.constant(room_room_distance)
+    model_ss_distance = model.constant(supply_supply_distance)
 
-# Output the results
+    #create two lists for permutations of rooms and supply closets
+    permutation_rooms = model.list(N_rooms)
+    permutation_supply = model.list(N_supply)
 
-for i in range(time_steps):
-    #cost: the cost of transporting goods as defined in the problem statement
-    cost = (model_flow[i][permutation_supply][:, permutation_rooms] * model_rs_distance).sum()
-    #room_penalty: the penalty cost of moving rooms (scales with distance traveled for each room)
-    room_penalty = (model_rr_distance[permutation_rooms][old_room_permutation]).sum()
-    #supply_penalty: the penalty cost of moving supply closets (also scales with distance)
-    supply_penalty = (model_ss_distance[permutation_supply][old_supply_permutation]).sum()
-    #minimize the total cost (finds a combination of a good layout without moving too many rooms)
-    model.minimize(cost + room_penalty + supply_penalty)
+    #store the previous permutation
+    old_room_permutation = model.list(N_rooms)
+    old_supply_permutation = model.list(N_supply)
 
-    #samples the model to determine the permutations of rooms and closets
-    sampler.sample(model)
-    with model.lock():
-        print(list(sym.state(0) for sym in model.iter_decisions()))
-        states = list(sym for sym in model.iter_decisions())
-        old_supply_permutation = states[3]
-        old_room_permutation = states[2]
-        print(model.objective.state(0))
+    sampler = LeapHybridNLSampler()
+
+    # Output the results
+
+    for i in range(time_steps):
+        #cost: the cost of transporting goods as defined in the problem statement
+        cost = (model_flow[i][permutation_supply][:, permutation_rooms] * model_rs_distance).sum()
+        #room_penalty: the penalty cost of moving rooms (scales with distance traveled for each room)
+        room_penalty = (model_rr_distance[permutation_rooms][old_room_permutation]).sum()
+        #supply_penalty: the penalty cost of moving supply closets (also scales with distance)
+        supply_penalty = (model_ss_distance[permutation_supply][old_supply_permutation]).sum()
+        #minimize the total cost (finds a combination of a good layout without moving too many rooms)
+        model.minimize(cost + room_penalty + supply_penalty)
+
+        #samples the model to determine the permutations of rooms and closets
+        sampler.sample(model)
+        with model.lock():
+            print(list(sym.state(0) for sym in model.iter_decisions()))
+            states = list(sym for sym in model.iter_decisions())
+            old_supply_permutation = states[3]
+            old_room_permutation = states[2]
+            print(model.objective.state(0))
+            cost += model.objective.state(0)
+    return cost
+
 
 
